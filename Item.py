f0a41b8c378074c73d4dcd399ad21e01074a2de9
Prattay Chowdhury <prattaychowdhury101@gmail.com>
2021-03-23 16:02:32 -0400

    Created Item class and constructor

---
+++ Item.py
@@ -0,0 +1,7 @@
+class Item:
+    def __init__(self, id, name, type, description, max_quantity):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.description = description
+        self.max_quantity = max_quantity
