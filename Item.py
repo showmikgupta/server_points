bc99a33aa54f7e4d00e232fb79bff9f7b3f8da3a
Showmik Gupta <showmikgupta@gmail.com>
2021-03-23 16:40:17 -0400

    added price attribute

--- Item.py
+++ Item.py
@@ -1,7 +1,8 @@
 class Item:
-    def __init__(self, id, name, type, description, max_quantity):
+    def __init__(self, id, name, price, type, description, max_quantity):
         self.id = id
         self.name = name
+        self.price = price
         self.type = type
         self.description = description
         self.max_quantity = max_quantity
