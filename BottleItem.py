4ee3e990e62dd7d2cf428768f80abc9ec7dd171f
Showmik Gupta <showmikgupta@gmail.com>
2021-04-02 18:10:18 -0400

    formatting

--- BottleItem.py
+++ BottleItem.py
@@ -1,5 +1,6 @@
 from Item import Item
 
+
 class BottleItem(Item):
     def __init__(self, id, name, price, type, description, max_quantity, probability, message, object):
         super().__init__(id, name, price, type, description, max_quantity, probability)
